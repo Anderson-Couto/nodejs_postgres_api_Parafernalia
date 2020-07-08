1c428b1b4ddcec70a01d0f80655d3b1ecb74c8f3
Anderson Couto Cardoso <andersoncouto57@gmail.com>
2020-07-08 19:49:02 -0300

    Atualizando o controller. API estruturada com todas as funcionalidades

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -14,8 +14,31 @@ exports.findProductById = async (req, res) => {
     'SELECT * FROM produtos WHERE id = $1',
     [idP],
   );
+  
+  // ==> Declarando as variáveis
+  const produtoPreco = responseP.rows[0].price;
+  const descontoBase = responseP.rows[0].base_discount_percent;
+  const aniversario = responseU.rows[0].birthdate;
+  const now = new Date;
+  const blackFriday = new Date(0, 10, 25);
+  let descontoTotal = descontoBase;
 
-  response = [responseP.rows[0], responseU.rows[0]] 
+  // ==> Regras de négocio
+  const regra1 = (aniversario.getDate() === now.getDate()) && (aniversario.getMonth() === now.getMonth());
+  const regra2 = (blackFriday.getDate() === now.getDate()) && (blackFriday.getMonth() === now.getMonth());
+  
+
+  // ==> Condições
+  if (regra1) { descontoTotal += 5.0 };
+  if (regra2) { descontoTotal += 10.0 };
+  if (descontoTotal > 25.0) { descontoTotal= 25.0 }
+
+  // ==> Aplicando desconto
+  const precoDescontado = produtoPreco*(100 - descontoTotal)/100
+
+  // ==> Retornando a resposta
+  const response = { "total_discount": descontoTotal,
+                    "final_price": precoDescontado}
 
   res.status(200).send(response);
 };
